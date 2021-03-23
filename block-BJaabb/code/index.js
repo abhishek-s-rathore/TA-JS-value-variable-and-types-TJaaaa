adc1418dfcb09237a820cc5fdb0a4c2540457697
Abhishek <abhishek.990914@gmail.com>
2021-03-23 15:12:40 +0530

    Assignment 3 Completed

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -6,21 +6,63 @@
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
 
+// let user = prompt(`Enter Your Name: `);
+// alert(user);
+
+// let userName = user;
+// console.log(user);
+// console.log(userName);
+
+// let age = 22,
+//   isLearning = true;
+
+// user = `John`;
+// console.log(user);
+// console.log(userName);
+
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+// let numA = Number(prompt(`Enter first number: `));
+// let numB = Number(prompt(`Enter second number: `));
+// let sum = numA + numB;
+// document.write(`Sum is ${sum}`);
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j++) {
+  if (j % 2 === 0) {
+    console.log(j);
+  }
+}
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
 
+for (let j = 10; j >= 0; j--) {
+  if (j % 2 !== 0) {
+    console.log(j);
+  }
+}
+
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+let string = "";
+for (let j = 5; j >= 0; j--) {
+  j = String(j);
+  string = string + j;
+}
+console.log(string);
 
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
+let string2 = "";
+for (let j = 10; j >= 0; j--) {
+  if (j % 2 === 0) {
+    j = String(j);
+    string2 = string2 + j;
+  }
+}
+console.log(string2);
 
 // Comparisoin
 
@@ -36,3 +78,12 @@ Example:
 "hello", -21; // false
 
 */
+
+let valueA = Number(prompt(`Enter first value: `));
+let valueB = Number(prompt(`Enter second value: `));
+
+if (valueA === valueB) {
+  alert(true);
+} else if (valueA !== valueB) {
+  alert(false);
+}
